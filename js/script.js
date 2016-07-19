3156d6bdb8e74ae7f6151af7e311988718398ee6
IMLaing <ianmlaing@gmail.com>
2016-07-19 18:47:35 -0400

    updated listener to use form

--- js/script.js
+++ js/script.js
@@ -4,41 +4,47 @@ console.log('ready!');
  
 $('#callContact').click(function(){
     console.log('callContact clicked');
-    var html = '<p> Name: '+ savedContact[0].newContact.firstName + ' ' + savedContact[0].newContact.lastName + '</p>'; 
-    html += '<p>Phone: '+ savedContact[0].newContact.phone +'</p>';
-    html += '<p>eMail: '+ savedContact[0].newContact.eMail +'</p>';
-    html += '<p> Address: '+ savedContact[0].newContact.street + ' ';
-    html += savedContact[0].newContact.city + ', ' +savedContact[0].newContact.state +'</p>';
-
+    var html = '<p> Name: '+ savedContacts[0].newContact.firstName + ' ' + savedContacts[0].newContact.lastName + '</p>'; 
+    html += '<p>Phone: '+ savedContacts[0].newContact.phone +'</p>';
+    html += '<p>eMail: '+ savedContacts[0].newContact.eMail +'</p>';
+    html += '<p> Address: '+ savedContacts[0].newContact.street + ' ';
+    html += savedContacts[0].newContact.city + ', ' +savedContacts[0].newContact.state +'</p>';
     $('.contactDisplay').html(html);
   });
 
-});
-var savedContact = [];
-
- var Contact = function(data){
-    this.firstName = data.firstName,
-    this.lastName = data.lastName,
-    this.phone = data.phone,
-    this.eMail = data.eMail,
-    this.street = data.street,
-    this.city = data.city,
-    this.state = data.state;
-  };
-
-
-var createNewContact = function(form){
+$('[name="contactForm"]').on('submit', function(){
     var newContact = new Contact({
-      firstName: form.firstName.value,
-      lastName: form.lastName.value,
-      phone: form.phone.value,
-      eMail: form.eMail.value,
-      street: form.street.value,
-      city: form.city.value,
-      state: form.state.value
+      firstName: this.firstName.value,
+      lastName: this.lastName.value,
+      phone: this.phone.value,
+      eMail: this.eMail.value,
+      street: this.street.value,
+      city: this.city.value,
+      state: this.state.value
     });
-    savedContact.push({newContact});  
-    var storageNumber = 0;
-    $('.contactList').append('<button id="' + storageNumber +'"> '+ newContact.firstName + ' ' + newContact.lastName+'</button><br>');
-    storageNumber++;  
+    savedContacts.push({newContact});    
+    $('.contactList').append('<button> '+ newContact.firstName + ' ' + newContact.lastName+'</button><br>'); 
+});
+
+});
+
+var savedContacts = [];
+var contactID = 0;
+
+var Contact = function(data){
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+    this.phone = data.phone;
+    this.eMail = data.eMail;
+    this.street = data.street;
+    this.city = data.city;
+    this.state = data.state;
+    this.id = contactID;
+    contactID++;
   };
+
+
+
+
+
+savedContacts.find(callback[id]);
