7aab68a7e54e6a2fc08358d2ffe36b903b8e9658
IMLaing <ianmlaing@gmail.com>
2016-07-21 18:08:36 -0400

    added alt address slider and properties

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
+
 $(document).ready(function(){
-  console.log('ready!');
-  
-  //display the additional phone number input
-  $('.toggle-button').on('click', function() {
-    console.log('clicked on togglePhone');
+console.log('ready!');
+ $('.toggle-button').on('click', function() {
     $('.hideAltPhone').toggle(500);
-  });
+});
+
+$('.toggle-buttonAddr').on('click', function() {
+    $('.hideAltAddr').toggle(500);
+});
   //display the information from the object that is stored in the array by accessing the data-contactID
-  $('.contactList').on('click', 'button', function(){
+$('.contactList').on('click', 'button', function(){
     console.log(this);
     var id = $(this).attr('data-contactID');
     console.log(id);
       // element is a contact inside the savedContacts array. find() itterates through the array and runs on  EVERY element.
-      var contact = savedContacts.find(function(element){
-        if (id == element.id){
-          return true;
-        }
-      });
-      console.log(contact);    
-      $('.contactDisplay').html(contact.displayDetails());
+    var contact = savedContacts.find(function(element){
+      if (id == element.id){
+        return true;
+      }
     });
-  //contact information is taken from the form on submit and passed in to the object constructor Contact
-  $('[name="contactForm"]').on('submit', function(event){
-    event.preventDefault();
-    var newContact = new Contact({
+    console.log(contact);    
+    $('.contactDisplay').html(contact.displayDetails());
+  });
+
+$('[name="contactForm"]').on('submit', function(event){
+  event.preventDefault();
+  var newContact = new Contact({
       firstName: this.firstName.value,
       lastName: this.lastName.value,
       phone: this.phone.value,
@@ -31,72 +33,59 @@ $(document).ready(function(){
       eMail: this.eMail.value,
       street: this.street.value,
       city: this.city.value,
-      state: this.state.value
+      state: this.state.value,
+    street2: this.street2.value,
+      city2: this.city2.value,
+      state2: this.state2.value
     });  
     savedContacts.push(newContact);    
     $('.contactList').append(newContact.displayButton()); 
-  });
+});
+
 });
 
 var savedContacts = [];
 var contactID = 0;
 
 var Contact = function(data){
-  this.firstName = data.firstName;
-  this.lastName = data.lastName;
-  this.phone = data.phone;
-  this.phone2 = data.phone2;
-  this.eMail = data.eMail;
-  this.street = data.street;
-  this.city = data.city;
-  this.state = data.state;
-  this.id = contactID;
-  contactID++; 
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+    this.phone = data.phone;
+    this.phone2 = data.phone2;
+    this.eMail = data.eMail;
+    this.street = data.street;
+    this.city = data.city;
+    this.state = data.state;
+  this.street2 = data.street2;
+    this.city2 = data.city2;
+    this.state2 = data.state2;
+    this.id = contactID;
+    contactID++; 
 };
 
 Contact.prototype.displayButton = function(){
-  return '<button data-contactID=" ' + this.id + ' "> '+ this.firstName + ' ' + this.lastName+ '</button><br>';
+  return '<button data-contactID=" ' + this.id + ' "> '+ this.firstName + ' ' + this.lastName+'</button><br>';
 };
 
 Contact.prototype.displayDetails = function(){
-  var html = '<p> Name: '+ this.firstName + ' ' + this.lastName + '</p>'; 
-  html += '<p>Phone: '+ this.phone +'</p>';
-  html += '<p>eMail: '+ this.eMail +'</p>';
-  html += '<p> Address: '+ this.street + ' ';
-  html += this.city + ', ' + this.state +'</p>';
-  html += '<p class="editButton" data-contactID=" ' + this.id + ' ">+Edit</p><br>';
+var html = '<p> Name: '+ this.firstName + ' ' + this.lastName + '</p>'; 
+    html += '<p>Phone: '+ this.phone +'</p>';
+    html += '<p>eMail: '+ this.eMail +'</p>';
+    html += '<p> Address: '+ this.street + ' ';
+    html += this.city + ', ' + this.state +'</p>';
   console.log(this.phone2);
   if (this.phone2 === "" || this.phone2 === null){
     console.log(this.phone2 + ' was empty!');
-    return html;
   } else {
-    console.log(this.phone2 + ' was not empty');
-    html += '<p>Alt Phone: '+ this.phone2 +'</p>';
-    return html;
+    console.log(this.phone2 + ' was not empty')
+        html += '<p>Alt Phone: '+ this.phone2 +'</p>';
   }
+   if (this.state2 === "" || this.state2 === null){
+    console.log(this.state2 + ' was empty!');
+  } else {
+    console.log(this.state2 + ' was not empty')
+        html += '<p>Alt state: '+ this.state2 +'</p>';
+  }
+  return html;
 };
 
-/*
-$('[name="editForm"]').on('submit', function(event){
-  event.preventDefault();  
-  var id = $(this).attr('data-contactID');
-  console.log(id);
-  var newEdit = function(){ for (var key in savedContacts[id]){
-    this.firstName = this.firstName.value,
-    this.lastName= this.lastName.value,
-    this.phone= this.phone.value,
-    this.phone2= this.phone2.value,
-    this.eMail= this.eMail.value,
-    this.street= this.street.value,
-    this.city= this.city.value,
-    this.state= this.state.value;
-  }
-};
-console.log(newEdit);
-$('.contactList').append(newEdit.displayButton());
-});  
-
-$(".contactDisplay").on('click', '.editButton', function() {
-    console.log('clicked on edit');
-    $('.hideEdit').toggle(500);
-  }); */
